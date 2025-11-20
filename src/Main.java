fa8d2ed47c999cf6633ab79e7d2c8094c0dbdb1c
ashleym979 <ashleym979@nycstudents.net>
2025-11-20 08:40:00 -0500

    Add files via upload
    11-20

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,46 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
         System.out.println("You will recieve 2-5 different letters and you'll have to guess what they are!");
+        System.out.println(" ");
         System.out.println("Please note that these words are just randomized letters and all lowercase");
+        System.out.println(" ");
         System.out.println("you have infinite tries :)");
-        System.out.println("");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("If you ever need a hint type HINT");
+        System.out.println(" ");
 
-        String word= RandomizedWord.random();
-
-        System.out.println("Your word is " +RandomizedWord.length()+" letters long");
+        int length= RandomizedWord.length();
+        String word= RandomizedWord.getRandomWord(length);
+        System.out.println(word);
+        System.out.println("Your word is " + length +" letters long");
 
         String userGuess="";
+        String backUp="";
         while (!(userGuess.equals(word)))
         {
             System.out.println("Make your guess: ");
+
             userGuess= scan.nextLine();
-            System.out.println(RandomizedWord.check(userGuess));
+            if (!("HINT".equals(userGuess)))
+            {
+                backUp=userGuess;
+            }
+
+            System.out.println(backUp);
+            System.out.println(userGuess);
+
+            if ("HINT".equals(userGuess))
+            {
+                System.out.println(RandomizedWord.hint(backUp, length));
+            }else
+            {
+                System.out.println(RandomizedWord.check(userGuess));
+            }
         }
 
         System.out.println("Congratulations! You got the word correct!");
--- src/RandomizedWord.java
+++ src/RandomizedWord.java
@@ -2,47 +2,82 @@ public class RandomizedWord {
     private static int wordLen;
     private static String randomWord;
 
-    public static int length()
-    {
-        wordLen= 4;
+    public static int length() {
+        wordLen = (int) ((Math.random() * 4) + 2);
 
         return wordLen;
     }
 
-    public static String random()
-    {
-        randomWord= "abcd";
+
+    public static String getRandomWord(int length) {
+        randomWord = "";
+        String alphabet = "abcdefghijklmnopqrstuwxyz";
+        for (int i = 0; i < length; i++) {
+            int num = (int) (Math.random() * 24);;
+            String character = alphabet.substring(num, (num + 1));
+            randomWord = randomWord + character;
+        }
         return randomWord;
     }
 
-    public static String check(String guess)
-    {
-        String checking= " ";
-        int h=0;
-        while (h<=wordLen-1) {
+    public static String check(String guess) {
+        StringBuilder checking = new StringBuilder(" ");
+        int h = 0;
+        while (h <= wordLen - 1) {
+            int count = 0;
             String b = guess.substring(h, (h + 1));
             for (int i = 0; i < randomWord.length(); i++) {
                 String a = randomWord.substring(i, (i + 1));
-                int count= 1;
                 if (b.equals(a)) {
                     if (i == h) {
-                        checking = (checking+b+" is in the correct spot. ");
-                    }else
-                    {
-                        checking = (checking+b+" is a letter in the word but its placement is wrong. ");
+                        checking.append(b).append(" is in the correct spot. ");
+                    } else {
+                        checking.append(b).append(" is a letter in the word but its placement is wrong. ");
                     }
-                } else if (((b.equals(a)) = false) && !(i == h)) {
+                } else {
                     count++;
                 }
-                if (!(b.equals(a)) && !(i == h)) {
-                }
-                if (count==wordLen){
-                    checking = (checking+b+" is not a letter in the word. ");
+
+                if (count == wordLen) {
+                    checking.append(b).append(" is not a letter in the word. ");
                 }
             }
             h++;
         }
-        return checking;
+        return checking.toString();
     }
 
+    public static String hint(String userWord, int length) {
+        String alphabet = "abcdefghijklmnopqrstuwxyz";
+        StringBuilder notGuessed = new StringBuilder();
+        String hints= "";
+        int h = 0;
+        while (h <= length - 1) {
+            int count = 0;
+            String b = userWord.substring(h, (h + 1));
+            for (int i = 0; i < randomWord.length(); i++) {
+                String a = randomWord.substring(i, (i + 1));
+                if (b.equals(a)) {
+                } else {
+                    count++;
+                }
+                if (count==length)
+                {
+                    notGuessed.append(a);
+                }
+            }
+            h++;
+        }
+
+        int ind= (int) (Math.random()*(notGuessed.length()-1));
+        for (int s=0;s<notGuessed.length()-1;s++)
+        {
+            String k = notGuessed.substring(s, (s + 1));
+            if ((notGuessed.indexOf(k))==ind)
+            {
+                hints=("the index of the missing letter in your word in the alphabet is: "+(alphabet.indexOf(k) + 1));
+            }
+        }
+        return hints;
+    }
 }
